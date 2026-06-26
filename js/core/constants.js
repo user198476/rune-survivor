bece07488638a569ab3605eff5b70185ebbb9924
user198476 <j.duval@oceancallcentre.com>
2026-06-26 13:17:16 +0200

    Remove skill tree tier

--- js/core/constants.js
+++ js/core/constants.js
@@ -30,7 +30,7 @@ const SPIKE_HEAL_LOCK_DURATION = 1.15;
 
 const SKILL_TIER_COST_MULTIPLIER = 2.8;
 const SKILL_TIER_EFFECT_BONUS = 0.25;
-const MAX_VISIBLE_SKILL_TIER = 2; // nombre de palier/tier max de l'arbre de compétence
+const MAX_VISIBLE_SKILL_TIER = 0; // nombre de palier/tier max de l'arbre de compétence
 
 // orange enemy
 const COWARD_SHOOTER_SPAWN_CHANCE = 0.18;
